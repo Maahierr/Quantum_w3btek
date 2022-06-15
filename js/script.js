56ce3127e5c818fa86cb7eae03c3133bfdf30597
Maahierr <maahierrkhanna06@gmail.com>
2022-06-15 19:49:29 +0530

    1

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const cursor = document.querySelector('.cursor');
+let a = document.querySelector('a');
+
+document.addEventListener('mousemove', (e) => {
+    let x = e.pageX;
+    let y = e.pageY;
+
+    cursor.style.top = y + 'px';
+    cursor.style.left = x + 'px';
+    cursor.style.display = 'block';
+});
+
+document.addEventListener('mouseout', () => {
+    cursor.style.display = 'none';
+});
+
+a.onmouseover
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      }else{
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}`
+      }
+    });
+  });
+};
+
+navSlide();
